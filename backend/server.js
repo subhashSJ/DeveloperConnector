32f377cf35136d1f307cc153cab321015961f511
subhashSJ <jadhavsubhash511@gmail.com>
2022-03-01 21:11:38 +0530

    user model added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middlewares
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("App is running..."));
 
 //Define Routes
