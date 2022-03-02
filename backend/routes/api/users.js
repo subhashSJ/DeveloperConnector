b077d481b943ac658565cb1597b79d04f9c1f326
subhashSJ <jadhavsubhash511@gmail.com>
2022-03-02 18:24:01 +0530

    user authentication added

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 //@route  POST api/users
 //@desc   Register user
@@ -15,14 +21,70 @@ router.post(
       "Plaese enter a passowrd with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Users route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists." }] });
+      }
+
+      //get user's gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", //default size
+        r: "pg", // reading => ensures absence of naked people
+        d: "mm", //default image
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Ecrypt password using bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id, //mongoose understands that it is _id
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
 module.exports = router;
+
+/* NOTE: add return if its not last res.send or res.status otherwise 
+        it will give error like headers are already been sent
+ */
